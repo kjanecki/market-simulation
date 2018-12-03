d5c9a7d148d8308de4fac192c35bdb7ef5665a54
krzysiek <janekjanecki14@gmail.com>
2018-12-03 21:20:48 +0100

    Add server and model improvements

--- model/MarketModel.py
+++ model/MarketModel.py
@@ -1,26 +1,35 @@
 # model2.py
-import random
 
 from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 
-from model.CommonCustomer import CommonCustomer
+from agents.CommonCustomer import CommonCustomer
+from Shop.Regal import Regal
+from agents.RegalAgent import RegalAgent
 
 
 class MarketModel(Model):
 
-    def __init__(self, agents_number, articles, width=50, height=50):
+    def __init__(self, agents_number, shop, width=50, height=50):
         self.running = True
-        self.articles = articles
+        self.shop = shop
         self.num_agents = agents_number
         self.schedule = RandomActivation(self)
         self.grid = MultiGrid(width, height, True)
+        self.regal_agents = []
 
-        for i in range(self.num_agents):
-            a = CommonCustomer(i, self, articles)
+        self.place_regals()
+
+        for i in range(self.num_agents + len(self.regal_agents)):
+            a = CommonCustomer(i, self, self.shop.articles)
             self.schedule.add(a)
             self.grid.place_agent(a, (a.x, a.y))
+
+    def place_regals(self):
+        for i in range(len(self.shop.regals.keys())):
+            shelf_agent = RegalAgent(i, self, list(self.shop.regals.values())[i])
+            self.grid.place_agent(shelf_agent, shelf_agent.get_location())
 
     def step(self):
         self.schedule.step()
--- model/Server.py
+++ model/Server.py
@@ -3,20 +3,49 @@ import random
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
+from Shop.Shop import Shop
+from Shop.Regal import Regal
+from agents.RegalAgent import RegalAgent
 from model.MarketModel import MarketModel
 
 article_dictionary = {}
 
+regals = {}
+regal_no = 0
+
+for j in range(8, 50, 6):
+    for i in range(5, 20):
+        regals[str(regal_no)] = Regal(regal_no, (i, j))
+        regal_no += 1
+        regals[str(regal_no)] = Regal(regal_no, (i, j-1))
+        regal_no += 1
+
+    for i in range(30, 45):
+        regals[str(regal_no)] = Regal(regal_no, (i, j))
+        regal_no += 1
+        regals[str(regal_no)] = Regal(regal_no, (i, j-1))
+        regal_no += 1
+
 for i in range(100):
-    article_dictionary[str(i)] = (random.randint(10, 40), random.randint(10, 40))
+    article_dictionary[str(i)] = random.randint(0, regal_no-1)
+
+shop = Shop(article_dictionary, regals, None)
 
 
 def agent_portrayal(agent):
-    portrayal = {"Shape": "circle",
-                 "Color": "red",
-                 "Filled": "true",
-                 "Layer": 0,
-                 "r": 0.5}
+    if type(agent) is RegalAgent:
+        portrayal = {"Shape": "rect",
+                     "Color": "black",
+                     "Filled": "true",
+                     "Layer": 0,
+                     "w": 0.9,
+                     "h": 0.9}
+    else:
+        portrayal = {"Shape": "circle",
+                     "Color": "red",
+                     "Filled": "true",
+                     "Layer": 0,
+                     "r": 0.5}
     return portrayal
 
 
@@ -25,4 +54,4 @@ grid = CanvasGrid(agent_portrayal, 50, 50, 500, 500)
 server = ModularServer(MarketModel,
                        [grid],
                        "Money Model",
-                       {"agents_number": 30, "width": 50, "height": 50, "articles": article_dictionary})
+                       {"agents_number": 30, "width": 50, "height": 50, "shop": shop})
