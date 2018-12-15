b9fcecdf207a9044fff7ac51998254463d89cb75
xkamson <marcinxkaminski@gmail.com>
2018-12-15 14:04:14 +0100

    Merging

--- agents/CommonCustomer.py
+++ agents/CommonCustomer.py
@@ -1,6 +1,7 @@
 import random
 
 from agents.Customer import Customer
+from agents.pathfinding import compute_astar_shortest_path
 
 
 class CommonCustomer(Customer):
@@ -8,49 +9,21 @@ class CommonCustomer(Customer):
     def __init__(self, unique_id, model, articles):
         super().__init__(unique_id, model, articles)
         self.next_article = ()
+        self.is_near_checkouts = False
+        self.checkout_agent = None
 
     def find_path(self):
         next_article_location = self.model.shop.regals[str(self.articles[str(self.shopping_list.pop(0))])].location
-        prev_pos = (self.x, self.y)
-        waypoints = self.compute_waypoints(prev_pos, next_article_location)
-        for next_pos in waypoints:
-            self.compute_rectlinear_path(prev_pos, next_pos)
-            prev_pos = next_pos
 
-    def compute_waypoints(self, start_position, end_position):
-        p = self.walk_along_regal(start_position, end_position)
-        p2 = self.walk_aside_regal(p, end_position)
-        p3 = (end_position[0], p2[1])
-        p4 = self.walk_to_shelf(end_position)
-
-        return [p, p2, p3, p4, p3]
-
-    def walk_along_regal(self,  start_position, end_position):
-        width = self.model.grid.width
-        rand = random.randint(-1, 1)
-        return round(width/2)+rand, start_position[1]
-
-    def walk_aside_regal(self, start_position, end_position):
-        i = 2
-        if end_position[1] % 2 != 0:
+        i = 1
+        if next_article_location[1] % 2 != 0:
             i = -1
-        return start_position[0], end_position[1] + i
 
-    def walk_to_shelf(self, end_position):
-        if end_position[1] % 2 != 0:
-            return end_position[0], end_position[1]-1
-        else:
-            return end_position[0], end_position[1]+1
-
+        target_point = (next_article_location[0], next_article_location[1] + i)
+        self.step_queue = compute_astar_shortest_path(self.model.space_graph, (self.x, self.y), target_point)
 
     def compute_rectlinear_path(self, p1, p2):
         x, y = p1[0], p1[1]
-        i = 1
-        if x > p2[0]:
-            i = -1
-        while x != p2[0]:
-            x += i
-            self.step_queue.append((x, y))
 
         i = 1
         if y > p2[1]:
@@ -59,11 +32,26 @@ class CommonCustomer(Customer):
             y += i
             self.step_queue.append((x, y))
 
+        i = 1
+        if x > p2[0]:
+            i = -1
+        while x != p2[0]:
+            x += i
+            self.step_queue.append((x, y))
+
     def move(self):
         next_step = self.step_queue.pop(0)
         self.x, self.y = next_step[0], next_step[1]
         self.model.grid.move_agent(self, (self.x, self.y))
 
-    def go_to_checkout(self):
-        print("Finished shopping")
-
+    def attempt_to_buy_products(self):
+        if not self.is_near_checkouts:
+            self.compute_rectlinear_path((self.x, self.y), (self.model.grid.width-3, self.y))
+            self.is_near_checkouts = True
+        elif self.checkout_agent is None:
+            self.checkout_agent = self.model.find_nearest_checkout((self.x, self.y))
+            self.compute_rectlinear_path((self.x, self.y), (self.x, self.checkout_agent.location[1]))
+        else:
+            pos = self.checkout_agent.stand_in_the_queue(self)
+            self.compute_rectlinear_path((self.x, self.y), pos)
+            self.is_waiting = True
--- agents/Customer.py
+++ agents/Customer.py
@@ -8,21 +8,30 @@ class Customer(Agent):
     def __init__(self, unique_id, model, articles):
         super().__init__(unique_id, model)
         self.articles = articles
-        self.shopping_list = generate_shopping_list(articles)
+        self.products_number = random.randint(1, 3)
         self.x = model.grid.width//2 + 1
         self.y = 0
         self.step_queue = []
+        self.is_waiting = False
+        self.is_checked = False
+        self.pos = (self.x, self.y)
+        self.shopping_list = generate_shopping_list(articles, self.products_number)
 
     def step(self):
-        if len(self.shopping_list) > 0:
+        if self.is_checked:
+            return
 
-            if len(self.step_queue) == 0:
+        if len(self.step_queue) != 0:
+            self.move()
+        elif len(self.shopping_list) > 0:
                 self.find_path()
-            else:
-
+                self.products_number += 1
                 self.move()
+        elif self.is_waiting and self.model.grid.width > self.x+1:
+            if self.model.grid.is_cell_empty((self.x+1, self.y)):
+                self.step_forward()
         else:
-            self.go_to_checkout()
+            self.attempt_to_buy_products()
 
     def find_path(self):
         pass
@@ -30,9 +39,24 @@ class Customer(Agent):
     def move(self):
         pass
 
-    def go_to_checkout(self):
+    def attempt_to_buy_products(self):
         pass
 
+    def check_up_products(self, n):
+        self.products_number -= n
+        if self.products_number <= 0:
+            self.finish_shopping()
 
-def generate_shopping_list(articles):
-    return random.sample(range(len(list(articles.keys()))), random.randint(3, 15))
+    def step_forward(self):
+        p = (self.x+1, self.y)
+        self.model.grid.move_agent(self, p)
+
+    def finish_shopping(self):
+        self.pos = (self.x, self.y)
+        if not self.is_checked:
+            # self.model.grid[self.x][self.y].remove(self)
+            self.is_checked = True
+
+
+def generate_shopping_list(articles, number):
+    return random.sample(range(len(list(articles.keys()))), number)
