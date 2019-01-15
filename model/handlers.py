2a9ec93f334615913eb0bb901d5762f770344089
xkamson <marcinxkaminski@gmail.com>
2019-01-15 20:39:04 +0100

    Almost finished. Added web handlinggggg

--- model/handlers.py
+++ model/handlers.py
@@ -1,10 +1,23 @@
 import tornado
 
+class BaseHandler(tornado.web.RequestHandler):
 
-class CustomersHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class CustomersHandler(BaseHandler):
 
     def initialize(self, market_buff):
         self.market_buff = market_buff
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
 
     def get(self):
         shopping_lists = {}
@@ -12,10 +25,13 @@ class CustomersHandler(tornado.web.RequestHandler):
             shopping_lists[agent.unique_id] = [product.name for product in agent.shopping_list]
         self.write(shopping_lists)
 
-class ColorChangeHandler(tornado.web.RequestHandler):
+class ColorChangeHandler(BaseHandler):
 
     def initialize(self, market_buff):
         self.market_buff = market_buff
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
 
     def get(self):
         agent_id = self.get_argument("id")
--- static/main.js
+++ static/main.js
@@ -1,23 +1,27 @@
 function addItemsToTable(items) {
+    console.log(items);
     if (items) {
         var oldTable = document.getElementById("table-body");
         var table = document.createElement('tbody');
         table.id = 'table-body';
 
         for (var i in items) {
+            console.log(i, items[i]);
             var row = table.insertRow(0);
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
-            cell1.innerHTML = '<strong class="uid" onClick = "highlightUser('+ items[i].id + ');" >'+ items[i].id + '</strong>';
-            cell2.innerHTML = items[i].shoppingList;
+            cell1.innerHTML = '<strong class="uid" onClick = "highlightUser(' + i + ');" >' + i + '</strong>';
+            cell2.innerHTML = items[i];
         }
         oldTable.parentNode.replaceChild(table, oldTable);
     }
 }
 
 function refresh() {
-    fetch('http://localhost:8080/users').then((res) => {
-        return res.json;
+    fetch('http://localhost:8521/users', {
+        mode: 'cors'
+    }).then((res) => {
+        return res.json();
     }).then(items => {
         addItemsToTable(items);
     }).catch(err => {
@@ -26,5 +30,7 @@ function refresh() {
 }
 
 function highlightUser(id) {
-    fetch('http://localhost:8080/users/'+id);
+    fetch('http://localhost:8521/color?id=' + id, {
+        mode: 'cors'
+    });
 }
